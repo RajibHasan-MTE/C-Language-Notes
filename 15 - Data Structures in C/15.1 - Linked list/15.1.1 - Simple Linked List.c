b8b05d2d07affb6cc967eb195bbf62be88ddfae9
Rajib Hasan <rajibhasan.mte@gmail.com>
2025-09-05 22:38:26 +0600

    add linked list

---
+++ 15 - Data Structures in C/15.1 - Linked list/15.1.1 - Simple Linked List.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node{
+
+    int data;
+    struct Node* next;
+
+
+};
+
+
+int main(){
+
+    //Create 3 nodes
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+
+    //Allocate memory
+    //head = (struct Node*)malloc(sizeof(struct Node));
+    //second = (struct Node*)malloc(sizeof(struct Node));
+    //third = (struct Node*)malloc(sizeof(struct Node));
+
+    //Assign values
+    head->data = 10;
+    head->next = second;
+
+
+    second->data = 20;
+    second->next = third;
+
+
+    third->data = 30;
+    third->next = NULL;
+
+
+    //Traverse list
+    struct Node* temp = head;
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+
+    return 0;
+
+}
