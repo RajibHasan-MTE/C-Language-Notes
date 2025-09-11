65c15fb2622fc2db91b18e4656632cdfc5a8118f
Rajib Hasan <rajibhasan.mte@gmail.com>
2025-09-12 01:22:54 +0600

    added doubly linked list

---
+++ 15 - Data Structures in C/15.1 - Linked list/15.1.3 - Doubly Linked list (DLL).c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+// Insert at end
+void insertEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+// Traverse forward
+void displayForward(struct Node* head) {
+    struct Node* temp = head;
+    printf("Forward: ");
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Traverse backward
+void displayBackward(struct Node* tail) {
+    struct Node* temp = tail;
+    printf("Backward: ");
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertEnd(&head, 10);
+    insertEnd(&head, 20);
+    insertEnd(&head, 30);
+
+    printf("Doubly Linked List:\n");
+    displayForward(head);
+
+    // Find last node
+    struct Node* tail = head;
+    while (tail->next != NULL)
+        tail = tail->next;
+
+    displayBackward(tail);
+
+    return 0;
+}
